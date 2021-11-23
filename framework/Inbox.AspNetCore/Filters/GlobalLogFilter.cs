a463f4d6b186df090856ab5096902551bd38ecb3
huangzhengeng <huangzg@hytch.com>
2021-11-23 18:02:20 +0800

    add Inbox.AspNetCore

---
+++ framework/Inbox.AspNetCore/Filters/GlobalLogFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inbox.AspNetCore.Filters
+{
+    /// <summary>
+    /// 全局日志过滤器
+    /// </summary>
+    public class GlobalLogFilter
+    {
+    }
+}
---
+++ framework/Inbox.AspNetCore/Middlewares/CorsMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Inbox.AspNetCore.Middlewares
+{
+    /// <summary>
+    /// 跨域中间件
+    /// </summary>
+    public class CorsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CorsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Headers.ContainsKey("Origin"))
+            {
+                context.Response.Headers.Add("Access-Control-Allow-Origin", context.Request.Headers["Origin"]);
+                context.Response.Headers.Add("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,HEAD,PATCH");
+                context.Response.Headers.Add("Access-Control-Allow-Headers", context.Request.Headers["Access-Control-Request-Headers"]);
+                context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+
+                if (context.Request.Method.Equals("OPTIONS"))
+                {
+                    context.Response.StatusCode = StatusCodes.Status200OK;
+                    return;
+                }
+            }
+
+            await _next(context);
+        }
+    }
+}
